0c4074efbf9727a06f58b385a10082103ade5474
isMbumbu <andrewambuka9@gmail.com>
2024-11-10 18:02:05 +0300

    update features

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
-function PlantCard() {
+function PlantCard({ plant }) {
+  const [soldOut, setSoldOut] = useState(false);
+
+  const handleToggleStock = () => setSoldOut(!soldOut);
+  
+
+  
+
   return (
-    <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
-      <p>Price: {"plant price"}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
-      ) : (
-        <button>Out of Stock</button>
-      )}
+    <li className="card">
+      <img src={plant.image} alt={plant.name} />
+      <h4>{plant.name}</h4>
+      <p>Price: {plant.price}</p>
+      <button onClick={handleToggleStock} className={soldOut ? "" : "primary"}>
+        {soldOut ? "Out of Stock" : "In Stock"}
+      </button>
     </li>
   );
 }
